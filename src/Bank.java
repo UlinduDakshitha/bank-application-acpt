bb3cdb9fce2175ba6d872f18ae23e974393d24ff
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-10-17 19:36:11 +0530

    update

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Bank {
 
     public static void main(String[] args) {
@@ -11,7 +10,6 @@ public class Bank {
         String[] accTypes = new String[10];
         double[] accBalances = new double[10];
         double[] loanAmounts = new double[10];
-        String[] loanDescriptions = new String[10];
 
         int accountCount = 0;
         int choice = 0;
@@ -22,15 +20,15 @@ public class Bank {
             System.out.println("2. Deposit Money");
             System.out.println("3. Withdraw Money");
             System.out.println("4. Check Balance");
-            System.out.println("5. Apply for Loan");
+            System.out.println("5. Apply Loan");
             System.out.println("6. View Loan Details");
-            System.out.println("7. Transfer Funds");
+            System.out.println("7. Transfer Money");
             System.out.println("8. Exit");
             System.out.print("Enter your choice: ");
             choice = sc.nextInt();
 
             if (choice == 1) {
-                accountCount = createAccount(sc, accNumbers, accNames, accTypes, accBalances, loanAmounts, loanDescriptions, accountCount);
+                accountCount = createAccount(sc, accNumbers, accNames, accTypes, accBalances, loanAmounts, accountCount);
             } else if (choice == 2) {
                 depositMoney(sc, accNumbers, accBalances, accountCount);
             } else if (choice == 3) {
@@ -38,151 +36,124 @@ public class Bank {
             } else if (choice == 4) {
                 checkBalance(sc, accNumbers, accNames, accTypes, accBalances, accountCount);
             } else if (choice == 5) {
-                applyLoan(sc, accNumbers, loanAmounts, loanDescriptions, accountCount);
+                applyLoan(sc, accNumbers, loanAmounts, accountCount);
             } else if (choice == 6) {
-                viewLoanDetails(sc, accNumbers, loanAmounts, loanDescriptions, accountCount);
+                viewLoan(sc, accNumbers, loanAmounts, accountCount);
             } else if (choice == 7) {
-                transferFunds(sc, accNumbers, accBalances, accountCount);
+                transferMoney(sc, accNumbers, accBalances, accountCount);
             } else if (choice == 8) {
-                System.out.println("Thank you for using the application!");
+                System.out.println("Thank you!...");
             } else {
-                System.out.println("Invalid choice!");
+                System.out.println("can't find");
             }
-
-
         }
-
     }
 
-    // ================= Methods for Each Feature =================
 
-    public static int createAccount(Scanner sc, String[] accNumbers, String[] accNames, String[] accTypes, double[] accBalances,
-                                    double[] loanAmounts, String[] loanDescriptions, int accountCount) {
+    public static int createAccount(Scanner sc, String[] accNumbers, String[] accNames,
+                                    String[] accTypes, double[] accBalances, double[] loanAmounts, int count) {
+
         System.out.println("Create Account");
-        System.out.print("Enter Account Holder Name:");
-        sc.next();
+        System.out.print("Enter Account Number: ");
+        String accNo = sc.next();
+
+        for (int i = 0; i < count; i++) {
+            if (accNumbers[i].equals(accNo)) {
+                System.out.println("Account already exists!");
+                return count;
+            }
+        }
+
+        System.out.print("Enter Name:");
         String name = sc.next();
 
-        System.out.print("Enter Account Number: ");
-        String accNo = sc.next();
+        System.out.println("Select Type: 1.Savings  2.Current");
+        int t = sc.nextInt();
+        String type;
 
-        // Check if account exists
-        boolean exists = false;
-        for (int i = 0; i < accountCount; i++) {
-            if (accNumbers[i].equals(accNo)) {
-                exists = true;
-                break;
-            }
-        }
-
-        if (exists) {
-            System.out.println("Account number already exists!");
-            return accountCount;
-        }
-
-        System.out.println("Select Account Type:");
-        System.out.println("1. Savings");
-        System.out.println("2. Current");
-        System.out.println("3. Wanitha Wasana");
-        System.out.print("Enter your choice: ");
-        int typeChoice = sc.nextInt();
-
-        String type = "Savings";
-        if (typeChoice == 2) {
+        if (t == 1) {
+            type = "Savings";
+        } else if (t == 2) {
             type = "Current";
-        } else if (typeChoice == 3) {
-            type = "Wanitha Wasana";
+        } else {
+            type = "Savings";
         }
 
-        System.out.print("Enter Initial Deposit: ");
+        System.out.print("Enter First Deposit: ");
         double balance = sc.nextDouble();
 
-        accNumbers[accountCount] = accNo;
-        accNames[accountCount] = name;
-        accTypes[accountCount] = type;
-        accBalances[accountCount] = balance;
-        loanAmounts[accountCount] = 0;
-        loanDescriptions[accountCount] = "";
+        accNumbers[count] = accNo;
+        accNames[count] = name;
+        accTypes[count] = type;
+        accBalances[count] = balance;
+        loanAmounts[count] = 0;
 
-        System.out.println("Account created successfully!");
-
-        return accountCount + 1;
+        System.out.println("Account create sucsessful!!");
+        return count + 1;
     }
 
-    public static void depositMoney(Scanner sc, String[] accNumbers, double[] accBalances, int accountCount) {
+    public static void depositMoney(Scanner sc, String[] accNumbers, double[] accBalances, int count) {
         System.out.println("Deposit Money");
         System.out.print("Enter Account Number: ");
-        sc.next();
         String accNo = sc.next();
 
-        int index = findAccount(accNumbers, accNo, accountCount);
-
+        int index = findAccount(accNumbers, accNo, count);
         if (index == -1) {
-            System.out.println("Account not found!");
+            System.out.println("Account not found");
             return;
         }
 
-        System.out.print("Enter Deposit Amount: ");
+        System.out.print("Enter Amount:");
         double amount = sc.nextDouble();
+        accBalances[index] += amount;
 
-        if (amount > 0) {
-            accBalances[index] = accBalances[index] + amount;
-            System.out.println("Deposit successful! New balance: " + accBalances[index]);
-        } else {
-            System.out.println("Invalid amount!");
-        }
+        System.out.println(" New Balance: " + accBalances[index]);
     }
 
-    public static void withdrawMoney(Scanner sc, String[] accNumbers, double[] accBalances, int accountCount) {
+    public static void withdrawMoney(Scanner sc, String[] accNumbers, double[] accBalances, int count) {
         System.out.println("Withdraw Money");
         System.out.print("Enter Account Number: ");
-        sc.next();
         String accNo = sc.next();
 
-        int index = findAccount(accNumbers, accNo, accountCount);
-
+        int index = findAccount(accNumbers, accNo, count);
         if (index == -1) {
             System.out.println("Account not found!");
             return;
         }
 
-        System.out.print("Enter Withdraw Amount: ");
+        System.out.print("Enter Amount: ");
         double amount = sc.nextDouble();
 
-        if (amount > 0 && amount <= accBalances[index]) {
-            accBalances[index] = accBalances[index] - amount;
-            System.out.println("Withdrawal successful! New balance: " + accBalances[index]);
+        if (amount <= accBalances[index]) {
+            accBalances[index] -= amount;
+            System.out.println("New Balance:" + accBalances[index]);
         } else {
-            System.out.println("Invalid amount or insufficient funds!");
+            System.out.println("Insufficient funds!");
         }
     }
 
     public static void checkBalance(Scanner sc, String[] accNumbers, String[] accNames, String[] accTypes,
-                                    double[] accBalances, int accountCount) {
+                                    double[] accBalances, int count) {
         System.out.println("Check Balance");
         System.out.print("Enter Account Number: ");
-        sc.next();
         String accNo = sc.next();
 
-        int index = findAccount(accNumbers, accNo, accountCount);
-
+        int index = findAccount(accNumbers, accNo, count);
         if (index == -1) {
             System.out.println("Account not found!");
         } else {
-            System.out.println("Account Holder: " + accNames[index]);
-            System.out.println("Account Type: " + accTypes[index]);
+            System.out.println("Name: " + accNames[index]);
+            System.out.println("Type: " + accTypes[index]);
             System.out.println("Balance: " + accBalances[index]);
         }
     }
 
-    public static void applyLoan(Scanner sc, String[] accNumbers, double[] loanAmounts, String[] loanDescriptions, int accountCount) {
-        System.out.println(" Apply for Loan");
+    public static void applyLoan(Scanner sc, String[] accNumbers, double[] loanAmounts, int count) {
+        System.out.println(" Apply Loan ");
         System.out.print("Enter Account Number: ");
-        sc.next();
-        String accNo = sc.nextLine();
+        String accNo = sc.next();
 
-        int index = findAccount(accNumbers, accNo, accountCount);
-
+        int index = findAccount(accNumbers, accNo, count);
         if (index == -1) {
             System.out.println("Account not found!");
             return;
@@ -190,63 +161,54 @@ public class Bank {
 
         System.out.print("Enter Loan Amount: ");
         double loan = sc.nextDouble();
-        sc.next();
-
-
         loanAmounts[index] = loan;
-
 
         System.out.println("Loan applied successfully!");
     }
 
-    public static void viewLoanDetails(Scanner sc, String[] accNumbers, double[] loanAmounts, String[] loanDescriptions, int accountCount) {
-        System.out.println("View Loan Details");
+    public static void viewLoan(Scanner sc, String[] accNumbers, double[] loanAmounts, int count) {
+        System.out.println(" View Loan");
         System.out.print("Enter Account Number: ");
-        sc.next();
         String accNo = sc.next();
 
-        int index = findAccount(accNumbers, accNo, accountCount);
-
+        int index = findAccount(accNumbers, accNo, count);
         if (index == -1) {
             System.out.println("Account not found!");
         } else {
             System.out.println("Loan Amount: " + loanAmounts[index]);
-
         }
     }
 
-    public static void transferFunds(Scanner sc, String[] accNumbers, double[] accBalances, int accountCount) {
-        System.out.println("Transfer Funds");
-        sc.next();
+    public static void transferMoney(Scanner sc, String[] accNumbers, double[] accBalances, int count) {
+        System.out.println("Transfer Money");
         System.out.print("Enter Sender Account Number: ");
-        String fromAcc = sc.next();
+        String from = sc.next();
 
         System.out.print("Enter Receiver Account Number: ");
-        String toAcc = sc.next();
+        String to = sc.next();
 
-        int fromIndex = findAccount(accNumbers, fromAcc, accountCount);
-        int toIndex = findAccount(accNumbers, toAcc, accountCount);
+        int i1 = findAccount(accNumbers, from, count);
+        int i2 = findAccount(accNumbers, to, count);
 
-        if (fromIndex == -1 || toIndex == -1) {
-            System.out.println("Invalid account number(s)!");
+        if (i1 == -1 || i2 == -1) {
+            System.out.println("Invalid account(s)!");
             return;
         }
 
-        System.out.print("Enter Transfer Amount: ");
+        System.out.print("Enter Amount: ");
         double amount = sc.nextDouble();
 
-        if (amount > 0 && amount <= accBalances[fromIndex]) {
-            accBalances[fromIndex] = accBalances[fromIndex] - amount;
-            accBalances[toIndex] = accBalances[toIndex] + amount;
+        if (amount <= accBalances[i1]) {
+            accBalances[i1] -= amount;
+            accBalances[i2] += amount;
             System.out.println("Transfer successful!");
         } else {
-            System.out.println("Invalid amount or insufficient funds!");
+            System.out.println("Insufficient balance!");
         }
     }
 
-    // Helper Method
-    public static int findAccount(String[] accNumbers, String accNo, int accountCount) {
-        for (int i = 0; i < accountCount; i++) {
+    public static int findAccount(String[] accNumbers, String accNo, int count) {
+        for (int i = 0; i < count; i++) {
             if (accNumbers[i].equals(accNo)) {
                 return i;
             }
